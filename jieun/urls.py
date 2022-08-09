a9a835839b5b5aeef708bda79f626947910e7ba9
Jieun-KWAK <jieun47631@gmail.com>
2022-08-10 01:00:31 +0900

    second commit

--- jieun/urls.py
+++ jieun/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from kwak import views
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
+
+    # html form을 이용해 블로그 객체 만들기
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
 ]
--- kwak/views.py
+++ kwak/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import BLog
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html') 
+
+# 블로그 글 작성 html 보여주는 함수
+def new(request):
+    return render(request, 'new.html') 
+
+# 블로그 글을 저장해주는 함수로 html render 불필요
+def create(request):
+    if(request.method == 'POST'):
+        post = BLog()
+        post.title = request.POST['title']
+        post.body = request.POST['body']
+        post.date = timezone.now() 
+        post.save()
+    return redirect('home')
